d1e19d7a51a3500f6f74b4351e01fe18f75734c6
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-02-07 03:49:21 +0200

    Login and Signup

--- Atoms/atoms.tsx
+++ Atoms/atoms.tsx
@@ -1,5 +1,13 @@
-import { atom } from 'jotai';
+import { atom } from "jotai";
 
-export const isLoggedIn = atom(localStorage.getItem("isLoggedIn") === "true" ? true : false);
+export const isLoggedIn = atom(
+  localStorage.getItem("isLoggedIn") === "true" ? true : false
+);
+export const name = atom(localStorage.getItem("name") || "");
+export const email = atom(localStorage.getItem("email") || "");
+export const password = atom(localStorage.getItem("password") || "");
+export const gender = atom(localStorage.getItem("gender") || "");
+export const age = atom(localStorage.getItem("age") || "");
+export const country = atom(localStorage.getItem("age") || "");
 
 // export const firstName = atom("")
